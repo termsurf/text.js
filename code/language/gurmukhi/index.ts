370e831d79ea0f054bf4cb5abd3d6098a79689ba
Lance Pollard <lp@elk.fm>
2023-10-13 18:57:42 -0700

    save

--- code/language/gurmukhi/index.ts
+++ code/language/gurmukhi/index.ts
@@ -1,95 +1,100 @@
 // https://en.wikipedia.org/wiki/Gurmukhi
 
-const x = {}
-export default x
-// import { build, transform } from '../base'
+import { Map, build, transform } from '../base'
 
-// const virama = '\u0A4d'
+export const virama = '\u0A4d'
 
-// const standaloneVowels = {
-//   ਅ: 'U',
-// }
+const standaloneVowels: Map = {
+  ਅ: 'U',
+}
 
-// const vowels = {
-//   '\u0A3e': 'aa',
-//   '\u0A3f': 'i',
-//   '\u0A40': 'ii',
-//   '\u0A41': 'u',
-//   '\u0A42': 'uu',
-//   '\u0A47': 'ee',
-//   '\u0A48': 'ai',
-//   '\u0A4b': 'oo',
-//   '\u0A4c': 'au',
-//   '\u0A4f': 'i',
-// }
+const vowels: Map = {
+  '\u0A3e': 'a_',
+  '\u0A3f': 'i',
+  '\u0A40': 'i_',
+  '\u0A41': 'u',
+  '\u0A42': 'u_',
+  '\u0A47': 'e_',
+  '\u0A48': 'ai',
+  '\u0A4b': 'o_',
+  '\u0A4c': 'au',
+  '\u0A4f': 'i',
+}
 
-// const vowelTransformer = Object.keys(vowels).reduce((m, x) => {
-//   let render = vowels[x]
-//   m[x] = m => {
-//     m[m.length - 1] = m[m.length - 1].replace(/U_?/, '') + render
-//   }
-//   return m
-// }, {})
+const vowelTransformer = Object.keys(vowels).reduce<Map>((m, x) => {
+  let render = vowels[x]
+  m[x] = m => {
+    const last = m[m.length - 1]
+    if (last) {
+      m[m.length - 1] = last.replace(/U[_-]?/, '') + render
+    } else if (typeof render === 'string') {
+      m[m.length - 1] = render
+    }
+  }
+  return m
+}, {})
 
-// const blank = {
-//   ੲ: '',
-//   ੳ: '',
-// }
+export const blank: Map = {
+  ੲ: '',
+  ੳ: '',
+}
 
-// const consonants = {
-//   ਕ: 'kU',
-//   ਖ: 'khU',
-//   ਖ਼: 'h+',
-//   ਗ: 'gU',
-//   ਗ਼: 'r~',
-//   ਘ: 'kU_',
+export const consonants: Map = {
+  ਕ: 'kU',
+  ਖ: 'kh~U',
+  ਖ਼: 'H',
+  ਗ: 'gU',
+  ਗ਼: 'G',
+  ਘ: 'kU-',
+  ਙ: 'qU',
+  ਚ: 'txU',
+  ਛ: 'txh~U',
+  ਜ: 'djU',
+  ਜ਼: 'z',
+  ਝ: 'txU-',
+  ਞ: 'ny~U',
+  ਟ: 'TU',
+  ਠ: 'Th~U',
+  ਡ: 'DU',
+  ਢ: 'TU-',
+  ਣ: 'NU',
+  ਤ: 'tU',
+  ਥ: 'th~U',
+  ਦ: 'dU',
+  ਧ: 'tU-',
+  ਨ: 'nU',
+  ਪ: 'pU',
+  ਫ: 'ph~U',
+  ਫ਼: 'fU',
+  ਬ: 'bU',
+  ਭ: 'pU-',
+  ਮ: 'mU',
+  ਯ: 'yU',
+  ਰ: 'rU',
+  ਲ: 'lU',
+  ਲ਼: 'LU',
+  ਵ: 'VU',
+  ਸ: 'sU',
+  ਸ਼: 'xU',
+  ਹ: 'hU',
+  ੜ: 'RU',
+}
 
-//   ਙ: 'qU',
-//   ਚ: 'txU',
-//   ਛ: 'txhU',
-//   ਜ: 'djU',
-//   ਜ਼: 'z',
-//   ਝ: 'txU_',
-//   ਞ: 'nyU',
-//   ਟ: 'TU',
-//   ਠ: 'ThU',
-//   ਡ: 'DU',
-//   ਢ: 'TU_',
-//   ਣ: 'NU',
-//   ਤ: 'tU',
-//   ਥ: 'thU',
-//   ਦ: 'dU',
-//   ਧ: 'tU_',
-//   ਨ: 'nU',
-//   ਪ: 'pU',
-//   ਫ: 'phU',
-//   ਫ਼: 'f',
-//   ਬ: 'bU',
-//   ਭ: 'pU_',
-//   ਮ: 'mU',
-//   ਯ: 'yU',
-//   ਰ: 'r!U',
-//   ਲ: 'lU',
-//   ਲ਼: 'L',
-//   ਵ: 'VU',
-//   ਸ: 'sU',
-//   ਸ਼: 'x',
-//   ਹ: 'hU',
-//   ੜ: 'RU',
-// }
+export const characters: Map = {
+  ...blank,
+  ...vowelTransformer,
+  ...standaloneVowels,
+  ...consonants,
+  [virama]: m => {
+    const last = m[m.length - 1]
+    if (last) {
+      m[m.length - 1] = last.replace(/U[_-]?/, '')
+    }
+  },
+}
 
-// const m = {
-//   ...blank,
-//   ...vowelTransformer,
-//   ...standaloneVowels,
-//   ...consonants,
-//   [virama]: m => {
-//     m[m.length - 1] = m[m.length - 1].replace(/U_?/, '')
-//   },
-// }
+const s = build(characters)
 
-// const s = build(m)
+const form = (t: string) => transform(t, s, characters)
 
-// const form = (t: string) => transform(t, s, m)
-
-// export default form
+export default form
